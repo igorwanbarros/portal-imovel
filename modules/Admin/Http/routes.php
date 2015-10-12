2f2ffe58d287443103e30e68a9269f976571c0d0
Igor Wanderley <igorwanbarros@gmail.com>
2015-10-12 17:21:28 -0300

    removendo rotas dos Crud's excluidos

--- modules/Admin/Http/routes.php
+++ modules/Admin/Http/routes.php
@@ -20,35 +20,4 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Modules\Admin\Http\Controller
 		Route::post('/store', 'ImovelImagemController@store');
 		Route::get('/{id}/destroy', 'ImovelImagemController@destroy');
 	});
-
-        //PAGE CONFIGURATION
-	Route::group(['prefix' => 'configuracoes'], function()
-	{
-		Route::get('/', 'PageConfigurationController@index');
-		Route::get('/novo', 'PageConfigurationController@create');
-		Route::get('/{id}/editar', 'PageConfigurationController@create');
-		Route::put('/{id}/update', 'PageConfigurationController@store');
-		Route::get('/{id}/remover', 'PageConfigurationController@destroy');
-		Route::post('/store', 'PageConfigurationController@store');
-	});
-
-	//COMPONENT
-	Route::group(['prefix' => 'configuracoes/componente'], function()
-	{
-		Route::get('/', 'ComponentController@index');
-		Route::get('/cadastrar', 'ComponentController@form');
-		Route::post('/store', 'ComponentController@store');
-		Route::get('/{id}/editar', 'ComponentController@form');
-		Route::get('/{id}/destroy', 'ComponentController@destroy');
-	});
-        
-	//INFOMATION COMPONENT
-	Route::group(['prefix' => 'configuracoes/componente/info'], function()
-	{
-		Route::get('/', 'InformationComponentController@index');
-		Route::get('/cadastrar', 'InformationComponentController@form');
-		Route::post('/store', 'InformationComponentController@store');
-		Route::get('/{id}/editar', 'InformationComponentController@form');
-		Route::get('/{id}/destroy', 'InformationComponentController@destroy');
-	});
 });
