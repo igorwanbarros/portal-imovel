a1cf6ba797bef01506a813dfed49814081001b03
Igor Wanderley <igorwanbarros@gmail.com>
2015-11-02 22:12:51 -0300

    atualizando arquivo composer e incluindo Seed users.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Model::unguard();
-
-//         $this->call(Modules\Admin\Database\Seeders\HeaderComponentTableSeeder::class);
-
-        Model::reguard();
+        DB::table('users')->insert([
+            'name'      => 'artisan',
+            'email'     => 'artisan@host.com',
+            'password'  => bcrypt('artisan'),
+        ]);
     }
 }
